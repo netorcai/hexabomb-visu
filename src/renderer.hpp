9d22cabd8d26c8a6279c16edf89de3078c13f9cb
Millian Poquet <millian.poquet@inria.fr>
2018-12-19 02:55:52 +0100

    [code] background = light gray

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -46,7 +46,7 @@ private:
     sf::View _boardView;
 
     const float _textureSize = 256.0f;
-    const sf::Color _backgroundColor = sf::Color(64, 64, 64);
+    const sf::Color _backgroundColor = sf::Color(0xa0a0a0ff);
     const sf::Vector2f _characterScale = sf::Vector2f(0.7f, 0.7f);
     const sf::Vector2f _bombScale = sf::Vector2f(0.5f, 0.5f);
 };
